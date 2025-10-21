4ae129d94e9c1c825c4148eb32a77acfe4f48ae8
Vamsi <vamsikrishna.burugadda@gmail.com>
2025-10-21 13:49:32 +0530

    Refactor Terraform configuration: uncomment ECS task/service definitions and update deployment workflow

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,12 +8,12 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.app.repository_url
 }
 
-# output "ecs_cluster_name" {
-#   description = "Name of the ECS cluster"
-#   value       = aws_ecs_cluster.main.name
-# }
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
 
-# output "ecs_service_name" {
-#   description = "Name of the ECS service"
-#   value       = aws_ecs_service.app.name
-# }
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.app.name
+}
